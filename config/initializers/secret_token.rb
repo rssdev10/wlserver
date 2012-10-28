9a66b6aec53c21a17861b24de9cec7761a43e11c
OpenSUSE <rss@linux-ci5n.(none)>
2012-10-28 15:34:15 +0400

    pure rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wlserver::Application.config.secret_token = 'ab92b45b0e130fb135426aea36c513280c5123fa1cf7eaf22ea0966b4c54c6d355e5d7d1fab984f374fed947820d61a26c706a067708336e1e5a8a7f47f3baff'
